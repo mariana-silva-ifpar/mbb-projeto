0bbaed361595ff43e877e702b6dc89428f8920c2
ketillybianca35 <ketillybianca35@gmail.com>
2025-12-15 10:34:44 -0300

    wip.

---
+++ src/app/features/photocards/photocards.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photocards',
+  imports: [],
+  templateUrl: './photocards.html',
+  styleUrl: './photocards.css',
+})
+export class Photocards {
+
+}
---
+++ src/app/interfaces/pc-page.ts
@@ -0,0 +1,5 @@
+export interface PhotocardPage {
+  id: number;
+  images: (string | null)[];
+  saved: boolean;
+}
